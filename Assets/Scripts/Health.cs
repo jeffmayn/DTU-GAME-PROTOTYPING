6acddbe8335b96a9cc6633ca4dc68b90d386dc46
jeffmayn <jeff@gyldenbrand.dk>
2020-12-13 02:41:27 +0100

    implemented attack system
    Not possible to attack enemies, and they loose HP too. Todo: make enemies stop within a range to a play such that they are not at exact same position (overlapping sprites)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+
+    public void TakeDamage(float dmg)
+    {
+        health = Mathf.Max(health - dmg, 0);
+        print(health);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,23 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
+  //  [SerializeField] Transform target;
+    [SerializeField] float swordDamage = 25f;
+    [SerializeField] Transform target;
 
     public int maxHealth = 5;
     public int minHealth = 0;
     public int speed = 2;
     public int jumpSpeed = 5;
+    [SerializeField] Vector2 dieJump = new Vector2(25f, 25f);
+
     bool isAlive = true;
+    bool canAttack = false;
 
     Rigidbody rigidbdy;
     Animator animator;
     CapsuleCollider collidr;
+    Ray lastRay;
 
   
 
@@ -41,9 +48,6 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-
-       
-
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             
@@ -64,12 +68,51 @@ public class PlayerController : MonoBehaviour
         Run();
         FlipSprite();
         Jump();
+        Die();
+        Raycast();
+        Attack();
+    }
 
+    private void Attack()
+    {
+        if (canAttack && Input.GetKeyDown(KeyCode.F) && target.tag == "Enemy")
+        {
+            print("Attacking: " + target);
+            animator.SetTrigger("Attack");
+            Health health = target.GetComponent<Health>();
+            health.TakeDamage(swordDamage);
+        }
+    }
+
+    private void Raycast()
+    {
+        Vector3 direction;
+        Vector3 origin = collidr.transform.position;
+
+        direction = new Vector3((collidr.transform.localScale.x), 0f, 0f);
+        Debug.DrawRay(origin, direction, Color.red);
+
+        Ray ray = new Ray(origin, direction);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 10f))
+        {
+            canAttack = true;
+            target = hit.transform;
+        }
+        else
+        {
+            canAttack = false;
+        }
     }
 
     private void Die()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            isAlive = false;
+            animator.SetTrigger("Dying");
+            GetComponent<Rigidbody>().velocity = dieJump;
+        }
     }
 
     private void FlipSprite()
